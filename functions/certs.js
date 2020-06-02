ee14e9a298bcd236bc2e8125441485871c538e2b
Denny Portillo <d3portillo@gmail.com>
2020-06-01 23:16:35 -0600

    includes functions

---
+++ functions/certs.js
@@ -0,0 +1,29 @@
+const fetch = require("node-fetch")
+const { TOKEN = false } = process.env
+const URL = `https://api.airtable.com/v0/appU8sXkXLRsB7KVe/AprendeJS%20-%20Certificados?view=List`
+exports.handler = (event, context, callback) => {
+  const voidData = () => {
+    const nothing = {
+      statusCode: 200,
+      body: JSON.stringify({}),
+    }
+    callback(null, nothing)
+  }
+  if (TOKEN) {
+    fetch(URL, {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    })
+      .then((r) => r.json())
+      .then(({ records }) =>
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(records),
+        })
+      )
+      .catch(voidData)
+  } else voidData()
+}
+
+console.log(exports.handler(0, 0, console.log))
