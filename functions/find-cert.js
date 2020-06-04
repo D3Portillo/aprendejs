32fc1408f23010bf0b7dca31c7d9f7563ff2d644
Denny Portillo <d3portillo@gmail.com>
2020-06-04 05:19:15 -0600

    redirects including for netlify hook trigger part 7

--- functions/find-cert.js
+++ functions/find-cert.js
@@ -1,9 +1,10 @@
 const fetch = require("node-fetch")
 const { TOKEN = false } = process.env
-const URL = `https://api.airtable.com/v0/appU8sXkXLRsB7KVe/AprendeJS%20-%20Certificados?view=List`
-const PDF_URL = `https://dl.airtable.com/.attachments/2c43383bddce36b4b4fdb2458fb95109/c17ea3ff/template.pdf`
+const BASE_URL = `https://api.airtable.com/v0/appU8sXkXLRsB7KVe/AprendeJS%20-%20Certificados`
 const NOT_FOUND_URL = `https://aprendejs.email/certificados/not-found/`
 exports.handler = (event, context, callback) => {
+  const id = event.QueryStringParameters["q"]
+  console.log({ id, event })
   const voidData = () => {
     const nothing = {
       statusCode: 302,
@@ -14,20 +15,23 @@ exports.handler = (event, context, callback) => {
     callback(null, nothing)
   }
   if (TOKEN) {
+    const URL = `${BASE_URL}/${id}`
     fetch(URL, {
       headers: {
         Authorization: `Bearer ${TOKEN}`,
       },
     })
       .then((r) => r.json())
-      .then(({ records }) =>
-        callback(null, {
-          statusCode: 302,
-          headers: {
-            Location: PDF_URL,
-          },
-        })
-      )
+      .then(({ fields }) => {
+        if (Array.isArray(fields.cert)) {
+          callback(null, {
+            statusCode: 302,
+            headers: {
+              Location: fields.cert[0].url,
+            },
+          })
+        } else voidData()
+      })
       .catch(voidData)
   } else voidData()
 }
