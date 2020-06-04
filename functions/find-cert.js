4e96b4543623e5f46e4a2fc026f6798c27c1ab87
Denny Portillo <d3portillo@gmail.com>
2020-06-04 05:24:22 -0600

    redirects including for netlify hook trigger part 8

--- functions/find-cert.js
+++ functions/find-cert.js
@@ -3,8 +3,9 @@ const { TOKEN = false } = process.env
 const BASE_URL = `https://api.airtable.com/v0/appU8sXkXLRsB7KVe/AprendeJS%20-%20Certificados`
 const NOT_FOUND_URL = `https://aprendejs.email/certificados/not-found/`
 exports.handler = (event, context, callback) => {
+  console.log({ event, context })
+
   const id = event.QueryStringParameters["q"]
-  console.log({ id, event })
   const voidData = () => {
     const nothing = {
       statusCode: 302,
